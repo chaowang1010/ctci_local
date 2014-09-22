925a11fb58f7aca20a37976300900bdd8a07f1ca
Chao Wang <dynastywang1010@gmail.com>
2014-09-22 19:03:43 -0400

    new file:   Java/Chapter_3/Question3_2/NodeWithMin.class 	new file:   Java/Chapter_3/Question3_2/NodeWithMin.java 	new file:   Java/Chapter_3/Question3_2/Question3_2.class 	new file:   Java/Chapter_3/Question3_2/Question3_2.java 	new file:   Java/Chapter_3/Question3_2/StackWithMin.class 	new file:   Java/Chapter_3/Question3_2/StackWithMin.java 	renamed:    Java/Chapter_3/Question3_2/Question2_3.java -> Java/Chapter_3/Questions_disc.java

---
+++ Java/Chapter_3/Question3_2/NodeWithMin.java
@@ -0,0 +1,9 @@
+class NodeWithMin {
+    public int value;
+    public int min;
+    // Constructor
+    public NodeWithMin(int v, int min){
+        value = v;
+        this.min = min;
+    }
+}
---
+++ Java/Chapter_3/Question3_2/Question3_2.java
@@ -0,0 +1,24 @@
+// 3.2
+// How would you design a stack which, in addition to push and pop, also has a
+// function min which returns the minimum element? Push, pop and min should
+// all operate in O(1) time
+
+import CtCILibrary.AssortedMethods;
+
+public class Question3_2{
+	public static void main(String[] args){
+		StackWithMin stack = new StackWithMin();
+		// StackWithMin2 stack2 = new StackWithMin2();
+		for (int i = 0; i < 15; i++) {
+			int value = AssortedMethods.randomIntInRange(0, 100);
+			stack.push(value);
+			//stack2.push(value);
+			System.out.print(value + ", ");
+		}
+		System.out.println('\n');
+		for (int i = 0; i < 15; i++) {
+			System.out.println("Popped " + stack.pop().value + ", "); // + stack2.pop()
+			System.out.println("New min is " + stack.min() + ", " );  // + stack2.min()
+		}
+	}
+}
